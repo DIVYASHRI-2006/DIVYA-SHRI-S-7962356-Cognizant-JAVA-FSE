0c446fb8e9432ecf0ffe65df1605d8e562a69dad
DIVYASHRI-2006 <divyashri5673@gmail.com>
2026-06-24 23:56:57 +0530

    Cognizant exercise

---
+++ Mandatory excercise/Week 1/Data Structures and algorithm/E-commerce platform/code/SearchDemo.java
@@ -0,0 +1,56 @@
+public class SearchDemo {
+
+    // Linear Search
+    public static Product linearSearch(Product[] products, int id) {
+        for (Product p : products) {
+            if (p.productId == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    // Binary Search
+    public static Product binarySearch(Product[] products, int id) {
+        int low = 0;
+        int high = products.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (products[mid].productId == id)
+                return products[mid];
+            else if (products[mid].productId < id)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        Product[] products = {
+                new Product(101, "Laptop", "Electronics"),
+                new Product(102, "Mobile", "Electronics"),
+                new Product(103, "Shoes", "Fashion"),
+                new Product(104, "Watch", "Accessories"),
+                new Product(105, "Bag", "Fashion")
+        };
+
+        int searchId = 104;
+
+        Product result1 = linearSearch(products, searchId);
+
+        System.out.println("Linear Search Result:");
+        if (result1 != null)
+            result1.display();
+
+        Product result2 = binarySearch(products, searchId);
+
+        System.out.println("Binary Search Result:");
+        if (result2 != null)
+            result2.display();
+    }
+    
+}
---
+++ Mandatory excercise/Week 1/Data Structures and algorithm/Financial forecasting/code/FinancialForecast.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class FinancialForecast {
+
+    public static double forecast(double currentValue,
+                                  double growthRate,
+                                  int years) {
+
+        if (years == 0)
+            return currentValue;
+
+        return forecast(currentValue * (1 + growthRate),
+                        growthRate,
+                        years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Current Value: ");
+        double currentValue = sc.nextDouble();
+
+        System.out.print("Enter Growth Rate (%): ");
+        double growthRate = sc.nextDouble() / 100;
+
+        System.out.print("Enter Number of Years: ");
+        int years = sc.nextInt();
+
+        double futureValue =
+                forecast(currentValue, growthRate, years);
+
+        System.out.printf("Future Value after %d years = %.2f",
+                years, futureValue);
+
+        sc.close();
+    }
+}
